92735e762e2d69cf5c3253853b495918dd146b81
Lauren Freeman <lfreeman124@gmail.com>
2022-05-03 11:13:36 +0200

    lightbox, transitions

--- gallery.js
+++ gallery.js
@@ -1,51 +1,41 @@
 let images = document.getElementById("images");
-window.onload = atmospheres();
 
-function atmospheres() {
+const galleries = {
+  atmos: {
+    numberOfPics: 8,
+    string: "atmos",
+  },
+  chair: {
+    numberOfPics: 20,
+    string: "chair",
+  },
+  table: {
+    numberOfPics: 10,
+    string: "table",
+  },
+  light: {
+    numberOfPics: 4,
+    string: "light",
+  },
+  other: {
+    numberOfPics: 13,
+    string: "other",
+  },
+};
+
+window.onload = makeGallery(galleries.atmos);
+
+function makeGallery(x) {
   images.textContent = "";
-  for (let i = 1; i < 9; i++) {
+  for (let i = 1; i <= x.numberOfPics; i++) {
+    const href = `./images/${x.string}/${x.string}${i}.png`;
     var a = document.createElement("a");
-    a.href = `./images/atmosphere/atmos${i}.png`;
+    a.href = href;
+    $(a).attr("data-lightbox", "mygallery");
     images.appendChild(a);
     var img = document.createElement("img");
-    img.src = `./images/atmosphere/atmos${i}.png`;
+    img.src = href;
     img.class = "lightbox";
     a.appendChild(img);
-  }
-}
-
-function chairs() {
-  images.textContent = "";
-  for (let i = 1; i < 21; i++) {
-    var img = document.createElement("img");
-    img.src = `./images/chairs/chair${i}.png`;
-    images.appendChild(img);
-  }
-}
-
-function tables() {
-  images.textContent = "";
-  for (let i = 1; i < 11; i++) {
-    var img = document.createElement("img");
-    img.src = `./images/table/table${i}.png`;
-    images.appendChild(img);
-  }
-}
-
-function lighting() {
-  images.textContent = "";
-  for (let i = 1; i < 5; i++) {
-    var img = document.createElement("img");
-    img.src = `./images/lighting/light${i}.png`;
-    images.appendChild(img);
-  }
-}
-
-function other() {
-  images.textContent = "";
-  for (let i = 1; i < 14; i++) {
-    var img = document.createElement("img");
-    img.src = `./images/other/other${i}.png`;
-    images.appendChild(img);
   }
 }
